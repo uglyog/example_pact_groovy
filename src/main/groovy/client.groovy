61a938dbdaa911d47b01adf795838c4c16acdd0a
Ronald Holshausen <rholshausen@dius.com.au>
2013-11-22 14:02:56 +1100

    pretty print the JSON

--- src/main/groovy/client.groovy
+++ src/main/groovy/client.groovy
@@ -1,16 +1,15 @@
-import groovyx.net.http.HTTPBuilder
+import groovyx.net.http.RESTClient
 
 
 def load_provider_json() {
   def date = new Date()
-  def http = new HTTPBuilder("http://localhost:8081/")
+  def http = new RESTClient("http://localhost:8081/")
   http.get(path: 'provider.json', query: ['valid_date': date]) { response, json ->
     if (response.success) {
-      println json.dump()
       json
     }
   }
 }
 
-println load_provider_json()
+println load_provider_json().toString(2)
 
